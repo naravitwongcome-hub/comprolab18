6016dd94aa8ace7d1a20d0cd46f238478eab3ff9
GAME <naravitwongcome@gmail.com>
2026-02-13 22:26:51 +0700

    lab18_1_complete

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm> 
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+
+struct Rect {
+    double x, y, w, h;
 };
 
-double overlap(){
+ Rect R1 = {1, 1, 5, 5};
+ Rect R2 = {2, 2, 5, 5};
 
+double overlap(Rect &R1, Rect &R2) {
+    double x_start = max(R1.x, R2.x);
+    double x_end = min(R1.x + R1.w, R2.x + R2.w);
+    double x_overlap = max(0.0, x_end - x_start);
 
+    double y_start = max(R1.y - R1.h, R2.y - R2.h);
+    double y_end = min(R1.y, R2.y);
+    double y_overlap = max(0.0, y_end - y_start);
 
+    return x_overlap * y_overlap;
+}
+
+int main() {
+    Rect R1 = {1, 1, 5, 5};
+ Rect R2 = {2, 2, 5, 5};
+   cout <<  overlap(R1, R2) ;
 }
